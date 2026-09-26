9d02f98c4f49d978abc06af40f7730124bb1f10e
akito0802 <light.of.king.a@gmail.com>
2026-09-26 15:58:10 +0900

    Connect EmiruTo to live push API

--- push-config.js
+++ push-config.js
@@ -1,3 +1,2 @@
 // EmiruTo background Web Push backend.
-// Supabase Edge Function companion endpoint.
-window.EMIRUTO_PUSH_SERVER = "https://nxjbhgohenblohmtvysk.supabase.co/functions/v1/emiruto-push";
+window.EMIRUTO_PUSH_SERVER = "https://emiruto-push-api.onrender.com";
