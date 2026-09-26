2c3e5acc7efbfa404654b37482070c4cd7bdb36c
akito0802 <light.of.king.a@gmail.com>
2026-09-26 10:08:52 +0900

    Force service worker activation and clear stale cache

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-const CACHE="emiruto-v3";
+const CACHE="emiruto-v4";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg","./assets/oshi/normal.jpg","./assets/oshi/happy.jpg","./assets/oshi/sad.jpg","./assets/oshi/night.jpg","./assets/oshi/rare.jpg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate",e=>e.waitUntil(
+  Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ])
+));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
   e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match("./index.html"))));
